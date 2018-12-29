9e67594ead1bb8f3b0087df55151e77cb3a6ebbc
Jekity <jekity@JekitydeiMac.local>
2018-12-29 14:10:02 +0800

    增加开屏广告控件

---
+++ MULaunchImageADView.h
@@ -0,0 +1,16 @@
+//
+//  MULaunchImageADView.h
+//  AFNetworking
+//
+//  Created by Jekity on 2018/12/29.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MULaunchImageADView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ MULaunchImageADView.m
@@ -0,0 +1,20 @@
+//
+//  MULaunchImageADView.m
+//  AFNetworking
+//
+//  Created by Jekity on 2018/12/29.
+//
+
+#import "MULaunchImageADView.h"
+
+@implementation MULaunchImageADView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
